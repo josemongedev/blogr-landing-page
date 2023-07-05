5d2501905533a77a9d652ebccffe3782e5589293
Jose Monge <info@josemonge.dev>
2023-07-04 20:29:31 -0600

    Completed state of infrastructure features section Missing circles svg background for blue container

--- src/components/Features/InfrastructureFeatures/InfrastructureFeatures.tsx
+++ src/components/Features/InfrastructureFeatures/InfrastructureFeatures.tsx
@@ -6,15 +6,18 @@ const InfrastructureFeatures = () => {
     <article className="features features__infrastructure ">
       <section className="infrastructure">
         <IllustrationPhones className="infrastructure__phone-svg" />
-        <h1 className="features__title infrastructure__title">
-          State of the Art Infrastructure
-        </h1>
-        <p className="features__description infrastucture__description">
-          With reliability and speed in mind, worldwide data centers provide the
-          backbone for ultra-fast connectivity. This ensures your site will load
-          instantly, no matter where your readers are, keeping your site
-          competitive.
-        </p>
+        <div className="infrastructure__content"></div>
+        <section className="infrastructure__text">
+          <h1 className="features__title infrastructure__title">
+            State of the Art Infrastructure
+          </h1>
+          <p className="features__description infrastructure__description">
+            With reliability and speed in mind, worldwide data centers provide
+            the backbone for ultra-fast connectivity. This ensures your site
+            will load instantly, no matter where your readers are, keeping your
+            site competitive.
+          </p>
+        </section>
       </section>
     </article>
   );
