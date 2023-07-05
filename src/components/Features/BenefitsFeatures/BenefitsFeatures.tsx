8b4a79a094ab567b4788637f64a6a32ce3736066
Jose Monge <info@josemonge.dev>
2023-07-04 21:50:16 -0600

    Finished footer and benefits sections

--- src/components/Features/BenefitsFeatures/BenefitsFeatures.tsx
+++ src/components/Features/BenefitsFeatures/BenefitsFeatures.tsx
@@ -1,9 +1,14 @@
+import { ReactComponent as IllustrationLaptopDesktop } from "../../../assets/illustration-laptop-desktop.svg";
+import { ReactComponent as IllustrationLaptopMobile } from "../../../assets/illustration-laptop-mobile.svg";
+
 import "./BenefitsFeatures.scss";
 
 const BenefitsFeatures = () => {
   return (
-    <article className="features features__benefits">
-      <section className="benefits">
+    <article className="features features__benefits benefits">
+      <IllustrationLaptopDesktop className="benefits__laptop-svg-desktop" />
+      <IllustrationLaptopMobile className="benefits__laptop-svg-mobile" />
+      <section className="benefits__text1">
         <h2 className="features__subtitle benefits__subtitle">
           Free, open, simple
         </h2>
@@ -14,7 +19,9 @@ const BenefitsFeatures = () => {
           commenting tools, and works seamlessly with Google Analytics. The
           architecture is clean and is relatively easy to learn.
         </p>
+      </section>
 
+      <section className="benefits__text2">
         <h2 className="features__subtitle benefits__subtitle">
           Powerful tooling
         </h2>
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,30 +5,37 @@ const Footer = () => {
   return (
     <footer className="footer">
       <BlogrLogo className="footer__brand" />
+      <div className="footer__content">
+        <section className="footer__category">
+          <h3 className="footer__title">Product</h3>
+          <ul className="footer__links">
+            <li>Overview</li>
+            <li>Pricing</li>
+            <li>Marketplace</li>
+            <li>Features</li>
+            <li>Integrations</li>
+          </ul>
+        </section>
 
-      <h3 className="footer__title">Product</h3>
-      <ul className="footer__links">
-        <li>Overview</li>
-        <li>Pricing</li>
-        <li>Marketplace</li>
-        <li>Features</li>
-        <li>Integrations</li>
-      </ul>
+        <section className="footer__category">
+          <h3 className="footer__title">Company</h3>
+          <ul className="footer__links">
+            <li>About</li>
+            <li>Team</li>
+            <li>Blog</li>
+            <li>Careers</li>
+          </ul>
+        </section>
 
-      <h3 className="footer__title">Company</h3>
-      <ul className="footer__links">
-        <li>About</li>
-        <li>Team</li>
-        <li>Blog</li>
-        <li>Careers</li>
-      </ul>
-
-      <h3 className="footer__title">Connect</h3>
-      <ul className="footer__links">
-        <li>Contact</li>
-        <li>Newsletter</li>
-        <li>LinkedIn</li>
-      </ul>
+        <section className="footer__category">
+          <h3 className="footer__title">Connect</h3>
+          <ul className="footer__links">
+            <li>Contact</li>
+            <li>Newsletter</li>
+            <li>LinkedIn</li>
+          </ul>
+        </section>
+      </div>
     </footer>
   );
 };
